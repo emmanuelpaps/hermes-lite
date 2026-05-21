acfb8117b45699268fa0784744d95d29003ef315
Emmanuel Padilla <emmanuelpadilla@Mac-mini-de-Emmanuel.local>
2026-05-21 14:07:28 -0600

    perf: add browserslist to remove legacy JS and enable optimizeCss

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Removido output: 'export' y unoptimized: true para aprovechar todo el poder de Vercel
+  experimental: {
+    optimizeCss: true
+  }
 };
 
 export default nextConfig;
